c094917f89c6f82264f99b09dc23773891c9b1ef
Bbaker1989 <86622638+Bbaker1989@users.noreply.github.com>
2022-01-16 23:37:39 -0500

    connect mysqldatabase

--- server.js
+++ server.js
@@ -1,12 +1,27 @@
+//import mysql2 package
+const mysql = require('mysql2');
+
 //import express
 const express = require('express');
 //PORT designation and APP expression
 const PORT = process.env.PORT || 3001;
 const app = express();
+
 //express.js middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-
+// conntect to database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        //Your MySQL username,
+        user: 'root',
+        //Your MySQL password
+        password: 'password123',
+        database: 'election'
+    },
+    console.log('Connected to the election database.')
+);
 
 
 //Default response for any other request (Not Found)
